26c066f44c8c2a44574165e29187da7e501ba839
gwen <gwen.thelin@gmail.com>
2023-04-04 11:35:13 -0600

    Added Fullscreen option

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -2,5 +2,16 @@ extends CanvasLayer
 
 signal options_back
 
+var is_fullscreen = false
+
+
 func _on_back_pressed():
 	emit_signal("options_back")
+
+
+func _on_fullscreen_pressed():
+	is_fullscreen = !is_fullscreen
+	if is_fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
